6d6b782ca922909018bc16a8c991f8bdf0ade257
ShadowWood <308346420@qq.com>
2017-02-08 21:24:42 +0800

    udpate gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
+'use strict';
+
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const watch = require('gulp-watch');
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('default', ['bulid', 'bulid realtime']);
+
+gulp.task('bulid', function() {
   return tsProject.src()
     .pipe(tsProject())
-    .js.pipe(gulp.dest("dist"));
+    .js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('bulid realtime', function() {
+  return watch('./src/**/*.ts', function() {
+    return tsProject.src()
+      .pipe(tsProject())
+      .js.pipe(gulp.dest('dist'));
+  })
 });
